7687fc34b326485b009bc8ef986790fe572a12f6
Devin Slick <devinslick@users.noreply.github.com>
2023-07-08 12:27:23 -0500

    Remove mp4 watcher; copy jpg to www/latest.jpg; webhook optional

--- wyze-motion-monitor/main.go
+++ wyze-motion-monitor/main.go
@@ -3,99 +3,141 @@ package main
 import (
 	"crypto/tls"
 	"encoding/json"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
-	"os/signal"
 	"path/filepath"
+	"sort"
 	"strings"
-	"sync"
-	"syscall"
 	"time"
 )
 
+// Payload represents the JSON payload sent in the webhook
 type Payload struct {
 	CameraName string `json:"cameraName"`
 	JPGPath    string `json:"jpgPath"`
-	MP4Path    string `json:"mp4Path"`
 }
 
-func main() {
-	// Check if the required command-line arguments are provided
-	if len(os.Args) < 3 {
-		log.Fatal("Usage: ./program <cameraName> <webhookURL>")
+// Function to send the payload as a webhook
+func sendPayload(webhookURL string, payload Payload) error {
+	if webhookURL == "" {
+		// Webhook URL not provided, skip sending the payload
+		return nil
 	}
 
-	cameraName := os.Args[1]
-	webhookURL := os.Args[2]
-
-	// Load the last sent payload from the saved file
-	lastPayload, err := loadLastPayload()
+	jsonPayload, err := json.Marshal(payload)
 	if err != nil {
-		log.Printf("Error while loading last payload: %v", err)
+		return err
 	}
 
-	// Create a channel to receive termination signals
-	terminate := make(chan os.Signal, 1)
-	signal.Notify(terminate, os.Interrupt, syscall.SIGTERM)
+	transport := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: transport}
 
-	// Create a wait group to wait for goroutines to finish
-	var wg sync.WaitGroup
+	// Use the custom HTTP client for sending the request
+	resp, err := client.Post(webhookURL, "application/json", strings.NewReader(string(jsonPayload)))
 
-	// Start monitoring image files
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		monitorFiles("/media/mmc/alarm", "*.jpg", cameraName, webhookURL, lastPayload)
-	}()
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
 
-	// Start monitoring video files
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		monitorFiles("/media/mmc/record", "*.mp4", cameraName, webhookURL, lastPayload)
-	}()
+	if resp.StatusCode != http.StatusOK {
+		return err
+	}
 
-	// Wait for termination signal
-	<-terminate
-
-	// Signal termination to goroutines
-	wg.Wait()
+	return nil
 }
 
-// Monitor files in the specified folder with the given extension
-func monitorFiles(folderPath, extension, cameraName, webhookURL string, lastPayload *Payload) {
-	var lastModified time.Time
+// Function to copy a file
+func copyFile(srcPath, destPath string) error {
+	srcFile, err := os.Open(srcPath)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+
+	destFile, err := os.Create(destPath)
+	if err != nil {
+		return err
+	}
+	defer destFile.Close()
+
+	_, err = io.Copy(destFile, srcFile)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Function to monitor the latest folder for new JPG files
+func monitorJPGFiles(folderPath, cameraName, webhookURL string) {
+	lastJPGPath := readLastJPGPath()
 
 	for {
-		files, err := filepath.Glob(filepath.Join(folderPath, "*", extension))
+		// Get the list of directories in the specified folder
+		dirList, err := getDirectories(folderPath)
 		if err != nil {
-			log.Printf("Error while searching for files: %v", err)
+			log.Printf("Error while getting directory list: %v", err)
+			continue
 		}
 
-		if len(files) > 0 {
-			latestFile := files[len(files)-1]
-			fileInfo, err := os.Stat(latestFile)
+		if len(dirList) > 0 {
+			// Sort the directories in descending order based on their names
+			sortDirectoriesDescending(dirList)
+
+			latestDir := dirList[0]
+			dirPath := filepath.Join(folderPath, latestDir)
+
+			// Get the list of JPG files in the latest directory
+			files, err := filepath.Glob(filepath.Join(dirPath, "*.jpg"))
 			if err != nil {
-				log.Printf("Error while getting file information: %v", err)
+				log.Printf("Error while searching for JPG files: %v", err)
 				continue
 			}
 
-			if fileInfo.ModTime().After(lastModified) {
-				lastModified = fileInfo.ModTime()
+			if len(files) > 0 {
+				// Sort the files to ensure they are in ascending order
+				sortFiles(files)
 
-				// Send JSON payload to webhook only if it's different from the last sent payload
-				payload := Payload{
-					CameraName: cameraName,
-					JPGPath:    latestFile,
-					MP4Path:    strings.Replace(latestFile, "/alarm/", "/record/", 1),
-				}
+				// Find the latest JPG file
+				latestJPGPath := files[len(files)-1]
 
-				if !isDuplicatePayload(payload, lastPayload) {
-					sendPayload(webhookURL, payload)
-					lastPayload = &payload
-					saveLastPayload(lastPayload)
+				if latestJPGPath != lastJPGPath {
+					lastJPGPath = latestJPGPath
+
+					// Copy the latest JPG file to /media/mmc/wz_mini/www/latest.jpg
+					err = copyFile(latestJPGPath, "/media/mmc/wz_mini/www/latest.jpg")
+					if err != nil {
+						log.Printf("Error while copying JPG file: %v", err)
+					} else {
+						log.Println("Copied latest JPG file to /media/mmc/wz_mini/www/latest.jpg")
+					}
+
+					// Send JSON payload to webhook if webhook URL is provided
+					if webhookURL != "" {
+						payload := Payload{
+							CameraName: cameraName,
+							JPGPath:    latestJPGPath,
+						}
+
+						err := sendPayload(webhookURL, payload)
+						if err != nil {
+							log.Printf("Error while sending payload to webhook: %v", err)
+						} else {
+							log.Printf("Sent webhook for JPG file: %s", latestJPGPath)
+						}
+					}
+
+					// Save the last JPG path to a file
+					err := saveLastJPGPath(lastJPGPath)
+					if err != nil {
+						log.Printf("Error while saving last JPG path: %v", err)
+					}
 				}
 			}
 		}
@@ -104,68 +146,82 @@ func monitorFiles(folderPath, extension, cameraName, webhookURL string, lastPayl
 	}
 }
 
-// Check if the payload is a duplicate of the last sent payload
-func isDuplicatePayload(payload Payload, lastPayload *Payload) bool {
-	return lastPayload != nil &&
-		payload.CameraName == lastPayload.CameraName &&
-		payload.JPGPath == lastPayload.JPGPath &&
-		payload.MP4Path == lastPayload.MP4Path
+// Function to read the last JPG path from a file
+func readLastJPGPath() string {
+	filePath := "/media/mmc/wz_mini/www/last_jpg_path.txt"
+	data, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return ""
+		}
+		log.Printf("Error while reading last JPG path file: %v", err)
+		return ""
+	}
+	return string(data)
 }
 
-// Load the last sent payload from the saved file
-func loadLastPayload() (*Payload, error) {
-	data, err := ioutil.ReadFile("last_payload.json")
-	if err != nil {
-		return nil, err
-	}
-
-	payload := &Payload{}
-	err = json.Unmarshal(data, payload)
-	if err != nil {
-		return nil, err
-	}
-
-	return payload, nil
-}
-
-// Save the last sent payload to the file
-func saveLastPayload(payload *Payload) error {
-	data, err := json.Marshal(payload)
+// Function to save the last JPG path to a file
+func saveLastJPGPath(path string) error {
+	filePath := "/media/mmc/wz_mini/www/last_jpg_path.txt"
+	err := ioutil.WriteFile(filePath, []byte(path), 0644)
 	if err != nil {
 		return err
 	}
-
-	err = ioutil.WriteFile("last_payload.json", data, 0644)
-	if err != nil {
-		return err
-	}
-
 	return nil
 }
 
-// Send JSON payload to the specified webhook URL
-func sendPayload(webhookURL string, payload Payload) {
-	jsonPayload, err := json.Marshal(payload)
+// Function to get the list of directories in the specified folder
+func getDirectories(folderPath string) ([]string, error) {
+	var dirList []string
+
+	err := filepath.Walk(folderPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			log.Printf("Error while walking directory: %v", err)
+			return nil
+		}
+
+		if info.IsDir() && path != folderPath {
+			dirList = append(dirList, info.Name())
+		}
+
+		return nil
+	})
+
 	if err != nil {
-		log.Printf("Error while marshaling JSON payload: %v", err)
-		return
+		return nil, err
 	}
 
-	transport := &http.Transport{
-	    TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-        }
-        client := &http.Client{Transport: transport}
+	return dirList, nil
+}
 
-        // Use the custom HTTP client for sending the request
-        resp, err := client.Post(webhookURL, "application/json", strings.NewReader(string(jsonPayload)))
+// Function to sort the directories in descending order based on their names
+func sortDirectoriesDescending(dirList []string) {
+	sort.Slice(dirList, func(i, j int) bool {
+		return dirList[i] > dirList[j]
+	})
+}
 
-	if err != nil {
-		log.Printf("Error while sending payload to webhook: %v", err)
-		return
+// Function to sort the files in ascending order
+func sortFiles(files []string) {
+	sort.Strings(files)
+}
+
+func main() {
+	// Retrieve command-line arguments
+	cameraName := os.Args[1]
+	var webhookURL string
+	if len(os.Args) > 2 {
+		webhookURL = os.Args[2]
 	}
-	defer resp.Body.Close()
 
-	if resp.StatusCode != http.StatusOK {
-		log.Printf("Webhook request failed with status: %s", resp.Status)
-	}
+	// Specify the folder path for JPG monitoring
+	jpgFolderPath := "/media/mmc/alarm"
+
+	log.Printf("Starting JPG monitoring for Camera: %s", cameraName)
+
+	// Start monitoring JPG files
+	go monitorJPGFiles(jpgFolderPath, cameraName, webhookURL)
+
+	// Wait indefinitely
+	select {}
 }
